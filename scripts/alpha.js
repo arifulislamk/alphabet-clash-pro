38fbd35fd3691b88d9ab2703e805380585a9445d
arifulislamk <ariful18374@gmail.com>
2024-02-14 19:38:53 +0600

    keyup . what is press player and some condition add

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -6,12 +6,38 @@
 //     playgroudSection.classList.remove('hidden')
 
 // }
+
+function handleKeyboradButtonpress(event){
+    // playerpress key 
+    const playerpressedkey = event.key ;
+    console.log('you press ' , playerpressedkey );
+
+    // expected key to pressed 
+    const screenAlphabets = document.getElementById('alphabet-screen') ;
+    const screen = screenAlphabets.innerText ;
+    const expectedKey = screen.toLowerCase();
+    console.log('beta eta chap' ,expectedKey);
+
+    // cheek 
+    if(expectedKey === playerpressedkey){
+        console.log('beta you win nameje ja')
+    }
+    else{
+        console.log('you loss.. taratari ja')
+    }
+}
+document.addEventListener('keyup' , handleKeyboradButtonpress );
+
+
+
 function continueGame(){
+    // get random alfabets
    const alphabet = randomAlphabets() ;
-    
+//    set alfabets in screnn
     const screenAlphabets = document.getElementById('alphabet-screen');
         screenAlphabets.innerText = alphabet ;
 
+ // setbackgruond
         setbackgroundColorById(alphabet);
 }
 function play(){
